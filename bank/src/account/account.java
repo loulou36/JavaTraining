27725186245abfc24a45498951312a51cb993600
LouLou <klogw36@gmail.com>
2022-05-26 20:19:36 +0200

    ACCOUNT - adding some basic methods and fields

--- bank/src/account/account.java
+++ bank/src/account/account.java
@@ -1,6 +1,36 @@
 package account;
 
-public class account {
-	public int id;
+import bank.Bank;
 
+public class account {
+	private String accNbr;
+	private String remarks;
+	private String accType;
+	private double balance;
+	private float yieldYearly;
+	private Bank bank;
+	public void openAccount() {
+		//create new account
+	}
+	public void closeAccount() {
+		//close account
+	}
+	public void showAccount() {
+		//show all account characteristics 
+		
+	}
+	public boolean searchAccount(String accNumber) {
+		//return true if account exists
+		
+		return false;
+	}
+	public void deposit(double sumDeposit) {
+		this.balance-=sumDeposit;
+		
+	}
+	public void withdrawal(double sumWithdrawal) {
+		this.balance+=sumWithdrawal;
+	}
+	
+	
 }
