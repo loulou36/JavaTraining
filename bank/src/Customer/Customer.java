8230a58298db77393e64ec9789f0c123c0f0b649
LouLou <klogw36@gmail.com>
2022-05-26 19:29:20 +0200

    add bank and customer class

---
+++ bank/src/Customer/Customer.java
@@ -0,0 +1,13 @@
+package Customer;
+
+public class Customer {
+	int id;
+
+	public Customer(int id) {
+		super();
+		this.id = id;
+	}
+	
+	
+
+}
---
+++ bank/src/bank/Bank.java
@@ -0,0 +1,11 @@
+package bank;
+import Customer.*;
+
+public class Bank {
+public static void main(String[] args) {
+	System.out.println("this is the main bank class");
+	//create new customer 
+	Customer customer1 = new Customer(1);
+	
+}
+}
